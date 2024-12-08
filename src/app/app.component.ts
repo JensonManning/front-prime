13ff42f53d9faa05c281a6edf76208c47547f2bc
JensonManning <157075196+JensonManning@users.noreply.github.com>
2024-12-08 11:11:16 -0700

    ngprime installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    standalone: true,
+    imports: [ButtonModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'front-prime';
+  
 }
